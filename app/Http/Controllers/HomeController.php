c1defbd18091c3fe3d9690827b15e4100ff457e4
wasao123 <jobhunting0414@gmail.com>
2022-08-01 21:08:36 +0900

    バグ修正

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -7,6 +7,7 @@ use App\Tactics;
 use App\Highlight;
 use App\Laser;
 use App\Charge;
+use Auth;
 
 class HomeController extends Controller
 {
@@ -28,13 +29,17 @@ class HomeController extends Controller
     public function index(Tactics $tactics,Highlight $highlight,Laser $laser,Charge $charge)
     {
         // $tactics = [DB::table(‘tactics’)->find(1)];
-        $tactics = $tactics->orderBy('updated_at', 'desc')->first();
-        //  dd($tactics->highlight->name);
-        return view('home')->with([
-            'tactics' =>$tactics,
-            'Highlight' => $highlight,
-            'Laser' => $laser,
-            'Charge' => $charge
-            ]);
+        if ($tactics->find(Auth::id()) === null){
+            $tactics = null;
+            }else{
+                $tactics = $tactics->find(Auth::id())->orderBy('updated_at', 'desc')->first();
+            }
+            //  dd($tactics->highlight->name);
+            return view('home')->with([
+                'tactics' =>$tactics,
+                'Highlight' => $highlight,
+                'Laser' => $laser,
+                'Charge' => $charge
+                ]);
     }
 }
